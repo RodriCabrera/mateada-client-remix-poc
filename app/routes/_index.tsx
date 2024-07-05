af337439ce52e0146703d479f54096cf2b5e8b85
Rodrigo <rodrigo1cabrera2@gmail.com>
2024-07-05 17:25:25 +0200

    chore: Update landing page title and links

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,46 +1,40 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import { Link, json } from "@remix-run/react";
+import { requireAuthCookie } from "~/auth";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "MateadApp" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const auth = requireAuthCookie(request);
+  return json({ auth });
+}
+
 export default function Index() {
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
+      <h1 className="text-3xl">MateadApp</h1>
       <ul className="list-disc mt-4 pl-6 space-y-2">
         <li>
-          <a
+          <Link
+            to={"meetups"}
             className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
           >
-            5m Quick Start
-          </a>
+            Go to meetups page
+          </Link>
         </li>
         <li>
-          <a
+          <Link
+            to={"login"}
             className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
+            reloadDocument
           >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
+            Login
+          </Link>
         </li>
       </ul>
     </div>
